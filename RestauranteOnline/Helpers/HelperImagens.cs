7f3f60812d35ab6cc4533e8e325ff515f6964e1b
Alexandre de Lima <alexandre-lima10@hotmail.com>
2016-04-19 21:22:32 -0300

    helpers funcionando

--- RestauranteOnline/Helpers/HelperImagens.cs
+++ RestauranteOnline/Helpers/HelperImagens.cs
@@ -11,12 +11,12 @@ namespace RestauranteOnline.Helpers
 
         public static MvcHtmlString ExibeImagens(this HtmlHelper hp)
         {
-            string str =    "<div style =\"width:100%; text-align:center; padding:10px\">" +
-                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block\"> "+
+            string str =    "<div style =\"width:100%; text-align:center; padding:10px \">" +
+                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block \"> "+
                             "<img src=\"Imagens/ComidaMineira.png\" /></div>" + 
-                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block\"> "+
+                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block \"> "+
                             "<img src=\"Imagens/Pizza.png\" /></div>" + 
-                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block\"> " +
+                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block \"> " +
                             "<img src=\"Imagens/Hamburger.png\" /></div>" +
                             "</div>";
             return new MvcHtmlString(str);
