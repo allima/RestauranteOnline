7a8bfc9beb798ea7250b235926d6e878dcf0957a
Alexandre de Lima <alexandre-lima10@hotmail.com>
2016-04-19 21:10:10 -0300

    adicionado helper das imagens

---
+++ RestauranteOnline/Helpers/HelperImagens.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestauranteOnline.Helpers
+{
+    public static class HelperImagens
+    {
+
+        public static MvcHtmlString ExibeImagens(this HtmlHelper hp)
+        {
+            string str =    "<div style =\"width:100%; text-align:center; padding:10px\">" +
+                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block\"> "+
+                            "<img src=\"Imagens/ComidaMineira.png\" /></div>" + 
+                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block\"> "+
+                            "<img src=\"Imagens/Pizza.png\" /></div>" + 
+                            "<div style=\"width:300px; height:168px; margin:5px; display:inline-block\"> " +
+                            "<img src=\"Imagens/Hamburger.png\" /></div>" +
+                            "</div>";
+            return new MvcHtmlString(str);
+        }
+
+    }
+
+}
